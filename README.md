29cb047526c7fe39d6bea7037244d4f4f9aa8375
William Fargues <william@bebeboutik.com>
2024-06-28 13:08:11 +0200

    a good solution have been found

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ The goal of this repository is to find the best way to create an Icon component 
 In the `components` folder, there are multiple variations of Icon components, each attempting a different technique. I've added comments to each of them explaining how they perform.
 
 As of now, a good solution has not been found.
+
+EDIT 28/06/2024: The `Icon5.tsx` is the best solution.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
-import { Icon1 } from "@/components/Icon1";
+// import { Icon1 } from "@/components/Icon1";
 // import { Icon2 } from "@/components/Icon2";
 // import { Icon3 } from "@/components/Icon3";
 // import { Icon4 } from "@/components/Icon4";
+import { Icon5 } from "@/components/Icon5";
 
 
 export default function Home() {
   return (
     <main>
       <p>Uncomment the icons you want to test and then build the app to analyze the bundle</p>
-      <Icon1 name="AMPOULE" style={{ width: 100, height: 100, fill: "green" }} />
+      {/* <Icon1 name="AMPOULE" style={{ width: 100, height: 100, fill: "green" }} /> */}
       {/* <Icon2 name="AMPOULE" style={{ width: 100, height: 100, fill: "yellow" }} /> */}
       {/* <Icon3 name="AMPOULE" style={{ width: 100, height: 100, fill: "blue" }} /> */}
       {/* <Icon4 name="AMPOULE" style={{ width: 100, height: 100, fill: "orange" }} /> */}
+      <Icon5.AMPOULE style={{ width: 100, height: 100, fill: "purple" }} />
     </main>
   );
 }
